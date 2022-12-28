4bedfe85fa366eb8aca95c8ca894fac8cb4302d9
yemoalakija <adeyemo.alakija@gmail.com>
2022-12-28 22:50:06 +0000

    redo

---
+++ assets/js/script.js
@@ -0,0 +1,41 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let buttons of buttons) {
+        buttons.addEventListener("click", function() {
+            if (this.getAttribute("data-type") ** "submit") {
+                alert("You clicked Sumbit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert('You clicked $(gameType)');
+            }
+    
+        })
+
+    }
+
+})
+
+function runGame() {
+}
+
+function checkAnswer() {
+}
+
+function calculateCorrectAnswer() {
+}
+
+function incrementScore() {
+}
+
+function incrementWrongAnswer() {
+}
+
+function displayAdditionQuestion() {
+}
+
+function displaySubtractQuestion() {
+}
+
+function displayMultiplyQuestion() {
+}
